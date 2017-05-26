9db24f154cda213651895d5c1bc6a504f3bbdfdd
Todd Fields <toddwfields@gmail.com>
2017-05-26 10:29:01 -0500

    Initial Commit

---
+++ weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  weather
+//
+//  Created by Todd Fields on 2017-05-26.
+//  Copyright © 2017 SKULLGATE Studios. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
